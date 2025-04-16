cb6796b5612d868fa4d083d26252e269e5caf8c2
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-16 16:56:16 +0200

    [UPD] aggiunto array di oggetti

--- app.js
+++ app.js
@@ -10,10 +10,58 @@ const port = 3000;
 app.use(express.static('public'));
 
 // definizione della rotta base
-app.get('/', (req, res)=>{
- res.type('html').send('<h1>Server del mio blog</h1>')
+app.get('/', (req, res) => {
+    res.type('html').send('<h1>Server del mio blog</h1>')
 })
 
-app.listen(port,() =>{
+// definizione della rotta bacheca
+app.get('/', (req, res) => {
+    const teams = [
+        {
+            titolo: "Juventus",
+            contenuto: "Giocatori",
+            immagine: "imgs/1484661620_q.webp",
+            tags: ["Serie A", "Calcio", "Torino"]
+        },
+        {
+            titolo: "Atalanta",
+            contenuto: "Giocatori",
+            immagine: "imgs/atalanta.png",
+            tags: ["Serie A", "Calcio", "Bergamo"]
+        },
+        {
+            titolo: "Bologna",
+            contenuto: "Giocatori",
+            immagine: "imgs/bologna.webp",
+            tags: ["Serie A", "Calcio", "Bologna"]
+        },
+        {
+            titolo: "Cagliari",
+            contenuto: "Giocatori",
+            immagine: "imgs/cagliari.png",
+            tags: ["Serie A", "Calcio", "Cagliari"]
+        },
+        {
+            titolo: "Fiorentina",
+            contenuto: "Giocatori",
+            immagine: "imgs/fiorentina.png",
+            tags: ["Serie A", "Calcio", "Fiorentina"]
+        },
+        {
+            titolo: "Inter",
+            contenuto: "Giocatori",
+            immagine: "imgs/inter.webp",
+            tags: ["Serie A", "Calcio", "Milano"]
+        },
+        {
+            titolo: "milan",
+            contenuto: "Giocatori",
+            immagine: "imgs/milan.webp",
+            tags: ["Serie A", "Calcio", "Milano"]
+        },
+    ]
+})
+
+app.listen(port, () => {
     console.log(`Personal blog server listening on port ${port}`);
 })
