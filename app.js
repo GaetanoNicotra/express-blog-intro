07bff75acf542eb2cb875eb8519f366c5ad74a1f
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-16 16:39:50 +0200

    [FEAT] impostato lo strumento per gli assets statici

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+// inizializzazione di express nella varibaile app
 const app = express();
 
+// definizioone della porta
 const port = 3000;
 
+// strumento per recuperare gli assets statici
+app.use(express.static('public'));
+
+// definizione della rotta base
 app.get('/', (req, res)=>{
  res.type('html').send('<h1>Server del mio blog</h1>')
 })
