526f034538c7db12f7cea077b1b231624e6d86a3
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-16 16:06:28 +0200

    [FEAT] aggiunta rotta base dell'app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{
+ res.type('html').send('<h1>Server del mio blog</h1>')
+})
+
+app.listen(port,() =>{
+    console.log(`server listening on port ${port}`);
+})
